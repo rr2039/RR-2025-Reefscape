e1fd1bc025f9d2f84e5993ae085dfb1e9a192254
LiamDark-Z <liammichaelellingson@gmail.com>
2025-02-17 18:52:23 -0600

    gyro and can

--- Swerve drive/src/main/java/frc/robot/Drivetrain.java
+++ Swerve drive/src/main/java/frc/robot/Drivetrain.java
@@ -4,29 +4,32 @@
 
 package frc.robot;
 
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.AnalogGyro;
+import com.studica.frc.AHRS.*;
 
 /** Represents a swerve drive style drivetrain. */
 public class Drivetrain {
   public static final double kMaxSpeed = 3.0; // 3 meters per second
   public static final double kMaxAngularSpeed = Math.PI; // 1/2 rotation per second
+  private final AHRS m_gyro = new AHRS(NavXComType.kMXP_SPI);
 
   private final Translation2d m_frontLeftLocation = new Translation2d(0.381, 0.381);
   private final Translation2d m_frontRightLocation = new Translation2d(0.381, -0.381);
   private final Translation2d m_backLeftLocation = new Translation2d(-0.381, 0.381);
   private final Translation2d m_backRightLocation = new Translation2d(-0.381, -0.381);
 
-  private final SwerveModule m_frontLeft = new SwerveModule(15, 26, 0, 1, 2, 3);
-  private final SwerveModule m_frontRight = new SwerveModule(13, 20, 4, 5, 6, 7);
-  private final SwerveModule m_backLeft = new SwerveModule(19, 18, 8, 9, 10, 11);
-  private final SwerveModule m_backRight = new SwerveModule(17, 21, 12, 13, 14, 15);
-
-  private final AnalogGyro m_gyro = new AnalogGyro(0);
+  private final SwerveModule m_frontLeft = new SwerveModule(1, 2, 0, 1, 2, 3);
+  private final SwerveModule m_frontRight = new SwerveModule(3, 4, 4, 5, 6, 7);
+  private final SwerveModule m_backLeft = new SwerveModule(5, 6, 8, 9, 10, 11);
+  private final SwerveModule m_backRight = new SwerveModule(7, 8, 12, 13, 14, 15);
 
   private final SwerveDriveKinematics m_kinematics =
       new SwerveDriveKinematics(
